9f1f3174b91283585c67765d7fadde0894cf83f0
byungju0 <byungju8797@naver.com>
2025-11-16 06:03:09 +0900

    fix

--- src/admin/admin.module.ts
+++ src/admin/admin.module.ts
@@ -5,9 +5,11 @@ import { UserModule } from 'src/user/user.module';
 import { AuthModule } from 'src/auth/auth.module';
 import { MentoringModule } from 'src/mentoring/mentoring.module';
 import { PaymentModule } from 'src/payment/payment.module';
+import { PaymentService } from 'src/payment/payment.service';
 
 @Module({
   imports: [QuizModule, UserModule, AuthModule, MentoringModule, PaymentModule],
   controllers: [AdminController],
+  providers: [PaymentService],
 })
 export class AdminModule {}
