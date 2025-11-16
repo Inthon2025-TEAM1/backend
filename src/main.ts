a22881ef67b39bdc10d066ccc9eaaffd82a20b08
byungju0 <byungju8797@naver.com>
2025-11-16 09:21:37 +0900

    cors

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: ['http://localhost:5173'], // Vite 개발 서버 주소
+    origin: ['*'], // Vite 개발 서버 주소
     credentials: true,
   });
   await app.listen(process.env.PORT ?? 3000);
